d37fb15f8408b941d47c29475c65b06bb086e03e
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2023-10-07 00:08:11 +0300

    add function of writing data

--- Binary representation/Binary representation/main.c
+++ Binary representation/Binary representation/main.c
@@ -6,18 +6,38 @@ int writeData(int* number1, int* number2)
     int amountOfValues;
     char check;
 
-    printf("Введите длину сортируемого массива: ");
+    printf("Введите первое число: ");
     amountOfValues = scanf_s("%d%c", number1, &check);
-    
+    if (amountOfValues != 2 || check != '\n')
+    {
+        printf("Некорректный ввод данных\n");
+        return 1;
+    }
+
+    printf("Введите второе число: ");
+    amountOfValues = scanf_s("%d%c", number2, &check);
+    if (amountOfValues != 2 || check != '\n')
+    {
+        printf("Некорректный ввод данных\n");
+        return 2;
+    }
+    return 0;
 }
 
 int tests(void)
 {
-
+    // здесь будут тесты
 }
 
 int main(void)
 {
     setlocale(LC_ALL,"Rus");
-	return 0;
+    int number1, number2;
+
+    const int errorWriteData = writeData(&number1, &number2);
+    if (!errorWriteData)
+    {
+        return 0;
+    }
+	return errorWriteData;
 }
