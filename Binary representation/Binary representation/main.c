44242f815a02e39ed8960c6e4a6f6743b4a678ee
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2023-10-07 21:19:48 +0300

    add functions of convertation to binary system and printing binary number

--- Binary representation/Binary representation/main.c
+++ Binary representation/Binary representation/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <locale.h>
+#include <stdlib.h>
+#include <math.h>
 
 int writeData(int* number1, int* number2)
 {
@@ -21,23 +23,56 @@ int writeData(int* number1, int* number2)
         printf("Ќекорректный ввод данных\n");
         return 2;
     }
+    printf("\n");
     return 0;
 }
 
-int tests(void)
+void showBinaryRepresentation(int numberOfDigits, int* binaryNumber)
 {
-    // здесь будут тесты
+    for (int i = 0; i < numberOfDigits; ++i)
+    {
+        printf("%d", binaryNumber[i]);
+    }
+    printf("\n");
+}
+
+void convertToBinary(int number, int* binaryNumber, int numberOfDigits)
+{
+    // переводим число в двоичную систему счислени€
+    int i = numberOfDigits - 1;
+
+    // выводим надпись, чтобы number сохранило свое значение перед сбросом до нул€
+    printf("число %d в двоичной системе счислени€ выгл€дит так: ", number);
+    while (number > 0)
+    {
+        binaryNumber[i] = number % 2;
+        number /= 2;
+        i--;
+    }
+
+    showBinaryRepresentation(numberOfDigits, binaryNumber);
 }
 
 int main(void)
 {
-    setlocale(LC_ALL,"Rus");
+    setlocale(LC_ALL, "Rus");
     int number1, number2;
 
     const int errorWriteData = writeData(&number1, &number2);
     if (!errorWriteData)
     {
+        // создаем массивы, длина которых равна количеству разр€дов в их двоичном виде
+        int numberOfDigits1 = (int)log2(number1) + 1;
+        int numberOfDigits2 = (int)log2(number2) + 1;
+        int* binaryNumber1 = (int*)calloc(numberOfDigits1, sizeof(int));
+        int* binaryNumber2 = (int*)calloc(numberOfDigits1, sizeof(int));
+
+        convertToBinary(number1, binaryNumber1, numberOfDigits1);
+        convertToBinary(number2, binaryNumber2, numberOfDigits2);
+
+        free(binaryNumber1);
+        free(binaryNumber2);
         return 0;
     }
-	return errorWriteData;
+    return errorWriteData;
 }
