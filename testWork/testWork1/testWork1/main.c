a7ce02f3545abec725e1d816ce0bd38e7444d088
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2023-10-27 15:23:51 +0300

    add test code, similar function and rework main working function

--- testWork/testWork1/testWork1/main.c
+++ testWork/testWork1/testWork1/main.c
@@ -1,43 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int* maxSum(const int* subsequence, int length)
+int findSumOfDigits(int number)
 {
-	int* answer = malloc(sizeof(int) * length);
-	int j = 0;
+	int absoluteNumber = number < 0 ? number * -1 : number;
+	int sum = 0;
+	while (absoluteNumber > 0)
+	{
+		sum += absoluteNumber % 10;
+		absoluteNumber /= 10;
+	}
+	return sum;
+}
+
+int* findNumbersWithMaxSumOfDigits(const int* numbers, int length, int* count)
+{
 	int maxSum = 0;
+	int* answer = malloc(length * sizeof(int));
+	int count1 = 0;
 
 	for (int i = 0; i < length; ++i)
 	{
-		int number = subsequence[i] < 0 ? subsequence[i] * -1: subsequence[i];
-		int sumOfDigits = 0;
-		while (number > 0)
+		const int digitSum = findSumOfDigits(numbers[i]);
+		if (digitSum > maxSum)
 		{
-			sumOfDigits += number % 10;
-			number /= 10;
+			maxSum = digitSum;
+			count1 = 0;
+			answer[count1] = numbers[i];
+			count1++;
 		}
-		if (sumOfDigits >= maxSum)
+		else if (digitSum == maxSum)
 		{
-			answer[j] = subsequence[i];
-			++j;
-			if (sumOfDigits > maxSum)
-			{
-				answer = malloc(sizeof(int) * length);
-				j = 0;
-				answer[j] = subsequence[i];
-				maxSum = sumOfDigits;
-			}
+			answer[count1] = numbers[i];
+			count1++;
 		}
 	}
+	*count = count1;
 	return answer;
 }
 
-void printNumbers(int* result, int length)
+bool similar(const int *numbers, const int *expected, const int length)
 {
 	for (int i = 0; i < length; ++i)
 	{
-		printf("%d ", result[i]);
+		if (numbers[i] != expected[i])
+		{
+			return false;
+		}
 	}
+	return true;
 }
 
 int tests(void)
@@ -50,19 +62,18 @@ int tests(void)
 									{124},
 									{124, 124},
 									{124, -107} };
+
 	for (int i = 0; i < 4; ++i)
 	{
+		int count = 0;
 		const int length = sizeof(testArray) / sizeof(testArray[0]);
-		const int* result = maxSum(testArray[i], length);
-		const int expectedResultsLength = sizeof(expectedResults) / sizeof(expectedResults[0]);
-		for (int j = 0; j < expectedResultsLength; ++j)
+		const int result = findNumbersWithMaxSumOfDigits(testArray[i], length, &count);
+		if (!similar(result, expectedResults[i], length))
 		{
-			if (result[j] != expectedResults[j])
-			{
-				return i + 1;
-			}
+			return i + 1;
 		}
 	}
+	return 0;
 }
 
 int main(void)
