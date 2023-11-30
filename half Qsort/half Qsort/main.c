f7d14d2ad72f148d80108eb99a4f45ddd39539ae
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2023-11-30 20:22:35 +0300

    rename functions; replace pivot with the first array element; change halfQsort function a little; remade tests

--- half Qsort/half Qsort/main.c
+++ half Qsort/half Qsort/main.c
@@ -1,64 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<locale.h>
+#include<stdbool.h>
 
-void swap(int* number1, int* number2);
-void halfQsort(int* arrayOfNumbers, int left, int right);
-void printArray(int* arrayOfNumbers, int length);
-int writeAndCheckData(int* arrayLength);
-void createArrayOfRandomNumbers(int* arrayOfNumbers, int length);
-void printResult(int* arrayOfNumbers, int length);
-int halfQsortTests(void);
-int arraysEqual(int* arrayOfNumbers, int* expected, int length);
-
-int main(void)
+int readAndCheckData(size_t* const arrayLength)
 {
-	setlocale(LC_ALL, "Rus");
-	const int arrayLength;
-	const int errorCode = writeAndCheckData(&arrayLength);
-	if (!errorCode)
+	printf("Input array length: ");
+	int scanResult = scanf_s("%Iu", arrayLength);
+	while (scanResult == 0)
 	{
-		const int errorHalfQsortTest = halfQsortTests();
-		if (!errorHalfQsortTest)
-		{
-			int* arrayOfNumbers = malloc(sizeof(int) * arrayLength);
-			createArrayOfRandomNumbers(arrayOfNumbers, arrayLength);
-			printResult(arrayOfNumbers, arrayLength);
-			free(arrayOfNumbers);
-			return 0;
-		}
-		return errorHalfQsortTest;
+		scanf_s("%*[^\n]");
+		printf("Invalid input! Please, enter the number: ");
+		scanResult = scanf_s("%Iu", arrayLength);
 	}
-	return errorCode;
-}
-
-int writeAndCheckData(int* arrayLength)
-{
-	// функция ввода и проверки данных
-	printf("Введите длину массива: ");
-	const int amountOfElements = scanf_s("%d", arrayLength);
-	if (amountOfElements != 1 || *arrayLength < 0)
+	if (*arrayLength < 0)
 	{
-		if (*arrayLength < 0)
-		{
-			printf("Ошибка: длина массива должна быть неотрицательным числом \n");
-			return 2;
-		}
-		else
-		{
-			printf("Ошибка: введены некорректные данные");
-			return 1;
-		}
+		return 1;
 	}
 	return 0;
 }
 
-void createArrayOfRandomNumbers(int* arrayOfNumbers, int length)
+void createArrayOfRandomNumbers(int* array, const size_t length)
 {
 	for (int i = 0; i < length; ++i)
 	{
-		arrayOfNumbers[i] = rand();
+		array[i] = rand() % 1000;
 	}
+}
+
+void printArray(int* array, const size_t arrayLength)
+{
+	for (int i = 0; i < arrayLength; ++i)
+	{
+		printf("%d ", array[i]);
+	}
+	printf("\n");
 }
 
 void swap(int* number1, int* number2)
@@ -68,121 +43,102 @@ void swap(int* number1, int* number2)
 	*number2 = temp;
 }
 
-void halfQsort(int* arrayOfNumbers, int left, int right)
+void halfQsort(int* array, const size_t length)
 {
-	// функция для перемещения чисел меньше опорного элемента в начало массива,
-	// а больше - в конец
-	int pivot = arrayOfNumbers[(int)((left + right) / 2)]; // опорный элемент для сравнения
-	printf("pivot = %d\n", pivot);
-	while (left <= right)
-	{
-		while (arrayOfNumbers[left] < pivot)
-		{
-			left++; // сдвигаем правый указатель, пока не найдем меньше pivot
-		}
-		while (arrayOfNumbers[right] > pivot)
-		{
-			right--; // сдвигаем левый указатель, пока не найдем больше pivot
-		}
+	const int pivot = array[0];
+	int smaller = 0;
 
-		if (left <= right)
+	for (size_t i = 1; i < length; ++i)
+	{
+		if (array[i] < pivot)
 		{
-			// меняем местами больший элемент массива с меньшим и сдвигаем указатели,
-			// чтобы не застрять в бесконечном цикле
-			swap(&arrayOfNumbers[left], &arrayOfNumbers[right]);
-			left++;
-			right--;
+			++smaller;
+			swap(&array[i], &array[smaller]);
 		}
 	}
+	swap(&array[0], &array[smaller]);
 }
 
-void printArray(int* arrayOfNumbers, int length)
+bool isCorrectArray(const int* const array, const size_t arrayLength, const int pivot)
 {
-	for (int i = 0; i < length; ++i)
+	bool shouldBeLess = true;
+	for (size_t i = 0; i < arrayLength; i++)
 	{
-		printf("%d ", arrayOfNumbers[i]);
-	}
-	printf("\n");
-}
-
-void printResult(int* arrayOfNumbers, int length)
-{
-	// выводим массив до и после выполнения кусорта
-	printf("Массив до: ");
-	printArray(arrayOfNumbers, length);
-	halfQsort(arrayOfNumbers, 0, length - 1);
-	printf("Массив после: ");
-	printArray(arrayOfNumbers, length);
-}
-
-int arraysEqual(int* arrayOfNumbers, int* expected, int length)
-{
-	// возвращает 1 при истине, в противном случае - 0
-	for (int i = 0; i < length; ++i)
-	{
-		if (arrayOfNumbers[i] != expected[i])
+		if (array[i] == pivot)
 		{
-			return 0;
+			shouldBeLess = false;
+		}
+		else if ((array[i] < pivot) != shouldBeLess)
+		{
+			return false;
 		}
 	}
-	return 1;
+	return true;
 }
 
-int halfQsortTests(void)
+void tests(bool* testResults)
 {
-	// Тест 1: Проверка сортировки массива из одного элемента
-	int array1[] = { 5 };
-	int expected1[] = { 5 };
-	halfQsort(&array1, 0, 0);
-	if (!arraysEqual(&array1, &expected1, 1))
+	int testArray1[] = { 3,1,4,1,5,9 };
+	halfQsort(testArray1, 6);
+	testResults[0] = isCorrectArray(testArray1, 6, 3);
+
+	int testArray2[] = { 5,4,3,2,1 };
+	halfQsort(testArray2, 5);
+	testResults[1] = isCorrectArray(testArray2, 5, 5);
+
+	int testArray3[] = { 1,2,3,4,5 };
+	halfQsort(testArray3, 5);
+	testResults[2] = isCorrectArray(testArray3, 5, 1);
+
+	int testArray4[] = { 1 };
+	halfQsort(testArray4, 1);
+	testResults[3] = isCorrectArray(testArray4, 1, 1);
+
+	return 0;
+}
+
+int main()
+{
+	bool testResults[4] = { false };
+	bool testsPassed = true;
+	tests(testResults);
+	for (size_t i = 0; i < 4; i++)
 	{
-		return 1;
+		if (!testResults[i])
+		{
+			printf("Test %Iu failed\n", i + 1);
+			testsPassed = false;
+		}
 	}
 
-	// Тест 2: Проверка сортировки массива с повторяющимися элементами
-	int array2[] = { 7,3,5,3,2 };
-	int expected2[] = { 2,3,3,5,7 };
-	halfQsort(&array2, 0, 4);
-	if (!arraysEqual(&array2, &expected2, 5))
+	if (!testsPassed)
 	{
+		return -1;
+	}
+
+	size_t arrayLength = 0;
+	const int errorInput = readAndCheckData(&arrayLength);
+	if (errorInput)
+	{
+		printf("Array length cannot be negative number\n");
+		return errorInput;
+	}
+	
+	int* array = (int*)calloc(arrayLength, sizeof(int));
+	if (array == NULL)
+	{
+		printf("Out of memory error\n");
 		return 2;
 	}
 
-	// Тест 3: Проверка сортировки упорядоченного по возрастанию массива
-	int array3[] = { 1,2,3,4,5 };
-	int expected3[] = { 1,2,3,4,5 };
-	halfQsort(&array3, 0, 4);
-	if (!arraysEqual(&array3, &expected3, 5))
-	{
-		return 3;
-	}
+	createArrayOfRandomNumbers(array, arrayLength);
+	printf("Array before: ");
+	printArray(array, arrayLength);
 
-	// Тест 4: Проверка сортировки упорядоченного по убыванию массива
-	int array4[] = { 5,4,3,2,1 };
-	int expected4[] = { 1,2,3,4,5 };
-	halfQsort(&array4, 0, 4);
-	if (!arraysEqual(&array4, &expected4, 5))
-	{
-		return 4;
-	}
+	halfQsort(array, arrayLength);
+	printf("Array after: ");
+	printArray(array, arrayLength);
 
-	// Тест 5: Проверка сортировки массива с нечетным количеством элементов
-	int array5[] = { 9,4,7,2,1 };
-	int expected5[] = { 1,4,2,7,9 };
-	halfQsort(&array5, 0, 4);
-	if (!arraysEqual(&array5, &expected5, 5))
-	{
-		return 5;
-	}
-
-	// Тест 6: Проверка сортировки массива с четным количеством элементов
-	int array6[] = { 6,3,8,2 };
-	int expected6[] = { 2,3,8,6 };
-	halfQsort(&array6, 0, 3);
-	if (!arraysEqual(&array6, &expected6, 4))
-	{
-		return 6;
-	}
-
+	free(array);
 	return 0;
 }
