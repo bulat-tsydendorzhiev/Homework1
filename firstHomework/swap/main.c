2531b4ee8a6fc3ab8e6c314615c66878c58c78e1
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2023-12-07 00:25:48 +0300

    remake swap because didn't see the task condition

--- firstHomework/swap/main.c
+++ firstHomework/swap/main.c
@@ -2,9 +2,9 @@
 
 void swap(int* number1, int* number2)
 {
-    int temp = *number1;
-    *number1 = *number2;
-    *number2 = temp;
+    *number1 ^= *number2;
+    *number2 ^= *number1;
+    *number1 ^= *number2;
 }
 
 int main()
