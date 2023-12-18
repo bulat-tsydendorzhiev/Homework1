6f024b619e9c2e14925ebeb54ad016fa29332551
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2023-12-18 23:11:21 +0300

    change method of counting sum of digits; fix small details

--- firstHomework/luckyTickets/main.c
+++ firstHomework/luckyTickets/main.c
@@ -2,25 +2,33 @@
 
 size_t countLuckyTickets(void)
 {
-	size_t result = 0, sumOfDigits = 0;
+	size_t result = 0;
+	size_t sumOfDigits = 0;
 	size_t sums[28] = { 0 };
 
-	for (size_t i = 0; i < 1000; ++i)
+	for (size_t i = 0; i < 10; ++i)
 	{
-		sumOfDigits = i / 100 + (i % 100) / 10 + i % 10;
-		sums[sumOfDigits]++;
+		for (size_t j = 0; j < 10; ++j)
+		{
+			for (size_t k = 0; k < 10; ++k)
+			{
+				++sums[i + j + k];
+			}
+		}
 	}
-	for (size_t i = 0; i < 14; ++i)
+
+	for (size_t i = 0; i < 28; ++i)
 	{
 		result += sums[i] * sums[i];
 	}
 
-	return 2 * result;
+	return result;
 }
 
 int main()
 {
     const size_t numberOfLuckyTickets = countLuckyTickets();
     printf("Six-digit number of lucky tickets = %Iu\n", numberOfLuckyTickets);
+
 	return 0;
 }
