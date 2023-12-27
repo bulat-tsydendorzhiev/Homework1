6b47d85fb98ac6fa8b904b970bc16e8af56e042e
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2023-12-27 16:21:26 +0300

    replace tabs with spaces

--- firstHomework/luckyTickets/main.c
+++ firstHomework/luckyTickets/main.c
@@ -2,27 +2,27 @@
 
 size_t countLuckyTickets(void)
 {
-	size_t result = 0;
-	size_t sumOfDigits = 0;
-	size_t sums[28] = { 0 };
+    size_t result = 0;
+    size_t sumOfDigits = 0;
+    size_t sums[28] = { 0 };
 
-	for (size_t i = 0; i < 10; ++i)
-	{
-		for (size_t j = 0; j < 10; ++j)
-		{
-			for (size_t k = 0; k < 10; ++k)
-			{
-				++sums[i + j + k];
-			}
-		}
-	}
+    for (size_t i = 0; i < 10; ++i)
+    {
+        for (size_t j = 0; j < 10; ++j)
+        {
+            for (size_t k = 0; k < 10; ++k)
+            {
+                ++sums[i + j + k];
+            }
+        }
+    }
 
-	for (size_t i = 0; i < 28; ++i)
-	{
-		result += sums[i] * sums[i];
-	}
+    for (size_t i = 0; i < 28; ++i)
+    {
+        result += sums[i] * sums[i];
+    }
 
-	return result;
+    return result;
 }
 
 int main()
@@ -30,5 +30,5 @@ int main()
     const size_t numberOfLuckyTickets = countLuckyTickets();
     printf("Six-digit number of lucky tickets = %Iu\n", numberOfLuckyTickets);
 
-	return 0;
+    return 0;
 }
