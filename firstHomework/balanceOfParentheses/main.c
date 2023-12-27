a54a27dd54b4062b93677c9a12815c3cd7c5f8ad
bulat-tsydendorzhiev <bulattsyden@gmail.com>
2023-12-27 16:22:40 +0300

    replace tabs with spaces

--- firstHomework/balanceOfParentheses/main.c
+++ firstHomework/balanceOfParentheses/main.c
@@ -5,32 +5,32 @@
 
 bool isBalanced(const char* const string)
 {
-	size_t numberOfOpenBrackets = 0;
-	for (size_t i = 0; string[i] != '\0'; ++i)
-	{
-		if (string[i] == '(')
-		{
-			++numberOfOpenBrackets;
-		}
-		else if (string[i] == ')')
-		{
-			if (numberOfOpenBrackets == 0)
-			{
-				return false;
-			}
-			--numberOfOpenBrackets;
-		}
-	}
-	return numberOfOpenBrackets == 0;
+    size_t numberOfOpenBrackets = 0;
+    for (size_t i = 0; string[i] != '\0'; ++i)
+    {
+        if (string[i] == '(')
+        {
+            ++numberOfOpenBrackets;
+        }
+        else if (string[i] == ')')
+        {
+            if (numberOfOpenBrackets == 0)
+            {
+                return false;
+            }
+            --numberOfOpenBrackets;
+        }
+    }
+    return numberOfOpenBrackets == 0;
 }
 
 int main()
 {
-	char string[MAX_STRING_LENGTH] = "";
-	printf("Enter string with parentheses (length of string less than %d characters): ", MAX_STRING_LENGTH);
-	scanf_s("%s", string, MAX_STRING_LENGTH);
+    char string[MAX_STRING_LENGTH] = "";
+    printf("Enter string with parentheses (length of string less than %d characters): ", MAX_STRING_LENGTH);
+    scanf_s("%s", string, MAX_STRING_LENGTH);
 
-	printf(isBalanced(string) ? "string is balanced\n": "string is not balanced\n");
+    printf(isBalanced(string) ? "string is balanced\n": "string is not balanced\n");
 
-	return 0;
+    return 0;
 }
